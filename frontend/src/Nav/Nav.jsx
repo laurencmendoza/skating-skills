57daa8de36ba836e137b8ed087ba84150488b790
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-12-08 07:16:34 -0800

    add nav

---
+++ frontend/src/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+import { BsPersonLinesFill } from "react-icons/bs";
+
+function Nav() {
+    return (
+        <header class="">
+            <a>Skating Skills</a>
+            <a>Practice Sessions</a>
+            <a>Tests</a>
+            <a>
+                <BsPersonLinesFill/>
+            </a>
+        </header>
+    )
+}
+
+export default Nav
